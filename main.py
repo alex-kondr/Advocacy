78f2345064d7d9d259200201bc91998e8770cef1
alex_kondr <alex_kondr@outlook.com>
2023-06-16 17:06:16 +0300

    save

---
+++ main.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+
+from src.routes import auth, contacts
+
+
+app_dir = Path(__file__).parent
+
+app = FastAPI()
+app.mount("/src/routes/static", StaticFiles(directory="src/routes/static"), name="routes_static")
+templates = Jinja2Templates(directory=app_dir / "src/routes/templates")
+
+app.include_router(auth.router)
+app.include_router(contacts.router)
+
+@app.get('/')
+def hello(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
