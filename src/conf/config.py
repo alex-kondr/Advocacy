47b3123524a2539ef609495762801b4fca8f9ea8
alex_kondr <alex_kondr@outlook.com>
2023-09-21 23:57:18 +0300

    save

--- src/conf/config.py
+++ src/conf/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    sqlalchemy_database_url: str = 'postgresql+psycopg2://user:password@localhost:5432/postgres'
+    sqlalchemy_database_url_advocacy: str = 'postgresql+psycopg2://user:password@localhost:5432/postgres'
     cloudinary_name: str = 'name'
     cloudinary_api_key: int = 12345678
     cloudinary_api_secret: str = 'api_secret'
